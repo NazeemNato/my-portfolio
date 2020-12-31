176539e62e6530aac555db9ff68ba71ee2c767d4
nazeemnato <nazeemnob18@gmail.com>
2020-12-31 19:44:18 +0530

    updated data

--- lib/data/body_para.dart
+++ lib/data/body_para.dart
@@ -1,13 +1,13 @@
-const String bodyPara = '''
-Dart & Python Developer. Studying at KTU UNI (CSE).Passionate about developing technology & Juventus.
-''';
+const String namePara = 'Ciao, I \'m  Nazeem!';
+
+const String bodyPara = '''Developer and web designer from Alappuzha, Kerala. I create custom websites and applications for fun and to help other people.''';
 
 const String exp_para = '''
 I have experience with many  tools and frameworks.My Development env : VS Code, Sublime,Windows Powershell,Android Studio,Jupyter Notebook etc...
+Native App Development, Web Development,Machine Learning and Data Science.
 ''';
 
 const String expr_para = '''
-Native App Development, Web Development,Machine Learning and Data Science.
 ''';
 
 const String intr_para = '''
@@ -16,8 +16,9 @@ Finance,Cyber Security,UI,UX and meme
 
 const String jdesigne_git = 'https://github.com/NazeemNato/japp';
 const String motiv_git = 'https://github.com/NazeemNato/Motiv-App';
-const String intrExp_git = 'https://github.com/NazeemNato/Internet-Explorer-BOT';
+const String intrExp_git =
+    'https://github.com/NazeemNato/Internet-Explorer-BOT';
 
-
-const String coming_soon_links  = 'https://coolkidproblems.com/wp-content/uploads/2017/06/coming-soon-1.jpg';
-const String intrExp_link  = 'https://twitter.com/internetExpBot';
+const String coming_soon_links =
+    'https://coolkidproblems.com/wp-content/uploads/2017/06/coming-soon-1.jpg';
+const String intrExp_link = 'https://twitter.com/internetExpBot';
