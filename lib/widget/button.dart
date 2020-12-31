ef4035b705e88b095423a2da61fb00932b3355e9
nazeemnato <nazeemnob18@gmail.com>
2020-12-31 19:45:41 +0530

    updated buttons

--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Widget fontButton({VoidCallback urlLink, IconData icons}) {
-  return MaterialButton(
-    onPressed: urlLink,
-    color: Colors.blue[300],
-    textColor: Colors.white,
-    padding: EdgeInsets.all(16),
-    shape: CircleBorder(),
-    child: Icon(icons),
+  return InkWell(
+    onTap: urlLink,
+    child: Container(
+      padding: EdgeInsets.all(16),
+      child: Icon(icons, color: Colors.white),
+    ),
   );
 }
 
@@ -26,11 +25,9 @@ Widget projectView({VoidCallback urlLink, IconData icons, String text}) {
       SizedBox(height: 10),
       Container(
           child: Text(
-         text,
-        style: GoogleFonts.exo(
-          color: Colors.black,
-          fontWeight: FontWeight.normal
-        ),
+        text,
+        style:
+            GoogleFonts.exo(color: Colors.black, fontWeight: FontWeight.normal),
       )),
     ],
   );
