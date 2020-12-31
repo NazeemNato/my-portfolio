4a60544cc6ae9cbe62c7599621ae7f17c5758e78
nazeemnato <nazeemnob18@gmail.com>
2020-12-31 19:45:24 +0530

    hola riverpod

---
+++ lib/riverpod/riverpod.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:webapp/data/body_para.dart';
+
+final bodyParaProvider = Provider((ref) => bodyPara);
+final expParaProvider = Provider((ref) => exp_para);
+final intrParaProvider = Provider((ref) => intr_para);
+final nameParaProvider = Provider((ref) => namePara);
